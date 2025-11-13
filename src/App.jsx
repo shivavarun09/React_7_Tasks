cd7b9eb43e4045269d0b8d78b39ae22cadfb2e87
shivavarun09 <varunapps09@gmail.com>
2025-11-13 17:42:55 +0530

    added Link to btn as compoent

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import { Route,Routes,Link } from 'react-router-dom'
 import Day1 from './Day1/Parent'
 import Day2 from './Day2/FormParent'
+import {Button, ButtonGroup} from '@mui/material'
 const App = () => {
   return (
     <div>
-      <Link to='/Day1'>Day1</Link>
-      <Link to='/Day2'>Day2</Link>
+      <ButtonGroup sx={{ gap: 2 }}>
+        <Button component={Link} to="/Day1" variant="contained">
+          Day1
+        </Button>
+        <Button component={Link} to="Day2" variant="contained">
+          Day 2
+        </Button>
+      </ButtonGroup>
 
       <Routes>
-        <Route path='/Day1' element={<Day1/>}/>
-        <Route path='Day2' element={<Day2/>}/>
+        <Route path="/Day1" element={<Day1 />} />
+        <Route path="Day2" element={<Day2 />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
